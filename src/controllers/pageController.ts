513ae4c797b63f40c14f12bf8196b588c8e9ecf0
Gabrielli M Feletti <gabriellimombrinii@gmail.com>
2023-03-13 12:38:31 -0300

    import layout

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,18 +1,17 @@
 import { Request, Response } from 'express';
 
 export const home = (req: Request, res: Response) => {
-    //res.render('pages/page');
-    res.send('home no controller');
+    res.send('pages/page');
 };
 
 export const dogs = (req: Request, res: Response) => {
-    //res.render('pages/page');
+    res.render('dogs');
 };
 
 export const cats = (req: Request, res: Response) => {
-    //res.render('pages/page');
+    res.render('cats');
 };
 
 export const fishes = (req: Request, res: Response) => {
-    //res.render('pages/page');
+    res.render('fishes');
 };
